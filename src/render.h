fc589b592d57542477912be5a70d4d4f7fb66d92
Manu Cornet <m@ma.nu>
2021-11-12 22:58:31 +0000

    Last little bit of reformatting, for now

--- src/render.h
+++ src/render.h
@@ -1,14 +1,12 @@
 #ifndef __RENDER_H__
 #define __RENDER_H__
 
-
 /*  Public functions  */
 
-gint32   render (gint32     image_ID,
-	       GimpDrawable       *drawable,
-	       PlugInVals         *vals,
-	       PlugInImageVals    *image_vals,
-	       PlugInDrawableVals *drawable_vals);
-
+gint32 render(gint32     image_ID,
+              GimpDrawable       *drawable,
+              PlugInVals         *vals,
+              PlugInImageVals    *image_vals,
+              PlugInDrawableVals *drawable_vals);
 
 #endif /* __RENDER_H__ */
--- src/texturize.h
+++ src/texturize.h
@@ -1,8 +1,8 @@
 // Counts number of cells != 0 in filled.
-int count_filled_pixels (guchar ** filled, int width_i, int height_i);
+int count_filled_pixels(guchar ** filled, int width_i, int height_i);
 
 // Compute the graph, cuts it and updates the image.
-void decoupe_graphe (
+void decoupe_graphe(
     int* patch_posn, // Where to put the patch.
     int width_i, int height_i, int width_p, int height_p,
     int channels,
@@ -13,7 +13,7 @@ void decoupe_graphe (
     gboolean make_tileable, gboolean invert);
 
 // Allocates the memory (with malloc) and fills with 0.
-guchar ** init_guchar_tab_2d (gint x, gint y);
+guchar ** init_guchar_tab_2d(gint x, gint y);
 
 
 /* Compute the best position to put the patch,
@@ -21,7 +21,7 @@ guchar ** init_guchar_tab_2d (gint x, gint y);
  * and     (x_patch_posn_max, y_patch_posn_max).
  */
 
-void offset_optimal (
+void offset_optimal(
     gint *resultat, // The position where the patch will have to be put.
     guchar *image, guchar *patch,
     gint width_p, gint height_p, gint width_i, gint height_i,
@@ -32,6 +32,6 @@ void offset_optimal (
     gboolean make_tileable);
 
 // Returns the minimal unfilled pixel under lexicographical order (y,x).
-int * pixel_to_fill (guchar ** filled, int width_i, int height_i, int* resultat);
+int * pixel_to_fill(guchar ** filled, int width_i, int height_i, int* resultat);
 
-gint modulo (gint x, gint m);
+gint modulo(gint x, gint m);
